8526b1da56bb4e91a97bc06872d64319030f6af7
DigitalDeveloperAM <achrafmehenneche.am@gmail.com>
2023-08-12 21:28:56 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
+
+
 function addingEventListener() {
+
+    const input = document.getElementById("button");
+    input.addEventListener("click", addingEventListener);
+    alert("I was clickded!");
 }
+
+addingEventListener();
